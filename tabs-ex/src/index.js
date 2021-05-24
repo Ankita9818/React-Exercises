2841c620979d7c4c389d178e6fae55975228d4c6
Ankita9818 <ankita.dixit@vinsol.com>
2021-05-24 15:47:03 +0530

    add exericse1

---
+++ tabs-ex/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Panel from './components/Panel';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Panel />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
